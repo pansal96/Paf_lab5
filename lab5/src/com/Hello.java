efe69c71fbad495cb70cea04c8e77fbc44eca3f8
pansal96 <sacheewa96@gmail.lk>
2021-04-19 11:09:26 +0530

    end the lab 5

--- lab5/src/com/Hello.java
+++ lab5/src/com/Hello.java
@@ -1,9 +1,8 @@
 package com;
 
-import javax.websocket.server.PathParam;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,13 +10,14 @@ import javax.ws.rs.core.MediaType;
 @Path("/Hello")
 public class Hello {
 
-//	@GET
-//	@Path("/")
-//@Produces(MediaType.TEXT_PLAIN)
-//public String hello()
-//{
-//return "Hello world.";
-// }
+	@GET
+	@Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
+  
+	public String hello()
+    {
+        return "Hello world.";
+    }
 	
 	@GET
 	@Path("/{name}")
